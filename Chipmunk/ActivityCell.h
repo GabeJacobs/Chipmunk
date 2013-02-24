4f63a38980dcb606db4cc41db350a3b24531a1d7
GabeJacobs <GabeJacobs824@gmail.com>
2013-02-24 03:13:40 -0500

    Created activity cell

---
+++ Chipmunk/ActivityCell.h
@@ -0,0 +1,17 @@
+//
+//  ActivityCell.h
+//  Chipmunk
+//
+//  Created by Gabe Jacobs on 2/24/13.
+//  Copyright (c) 2013 Gabe Jacobs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActivityCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *activityName;
+@property (weak, nonatomic) IBOutlet UIImageView *activityImage;
+@property (weak, nonatomic) IBOutlet UILabel *activityTime;
+@property (weak, nonatomic) IBOutlet UIImageView *activityTypeIcon;
+
+@end
